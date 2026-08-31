80f51c41c8057de23adaf538f3e7a8e093d545e5
parammagicmoments-create <parammagicmoments@gmail.com>
2026-08-30 22:01:04 -0400

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolution {
+    useCache = true
+}
+rootProject.name = "ImageEditorApp"
+include(":app")
